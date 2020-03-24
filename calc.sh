0c6ecb813313ec6993494b8ac79173bbfcdfdb86
Fabio <fabiog@live.com.pt>
2020-03-23 23:18:39 -0400

    loop the menu and allow user to exit the program

--- calc.sh
+++ calc.sh
@@ -2,6 +2,8 @@
 # When the program is first loaded, display a greeting to the user.
 echo "Hello, welcome to the calculator program."
 
+
+while true ;do
 # Then, display a menu that outlines the possible operations:
   # Add
   # Subtract
@@ -18,23 +20,24 @@ read selection
 case $selection in
 # If the selection matches a supported operation, execute the operation.
 "Add")
-echo "Please first the first number'"
+echo "Please enter the first number"
 read first_number
-echo "Please second the second number"
+echo "Please enter the second number"
 read second_number
 echo "The sum of $first_number and $second_number is:"
 expr $first_number + $second_number
 ;;
 "Subtract")
-echo "Please first the first number'"
+echo "Please enter the first number'"
 read first_number
-echo "Please second the second number"
+echo "Please enter the second number"
 read second_number
 echo "The difference of $first_number and $second_number is:"
 expr $first_number - $second_number
 ;;
 "Exit")
-echo "user has selected 'Exit'"
+echo "Thanks for using the calculator, Goodbye."
+exit 0
 ;;
 # If the selection does not match a support operation, display an error message.
 *)
@@ -42,5 +45,6 @@ echo "Invalid selection:$selection"
 echo "Please try again"
 
 esac
+done
 
-# When the operation is complete, redisplay the menu.
+
